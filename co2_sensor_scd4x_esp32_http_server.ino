da3c9b12d5ed87df5bb402480ff58be4b06aa62e
sighmon <simon.loffler@gmail.com>
2022-07-26 18:31:15 +0930

    Set the character set to UTF-8, and remove LED http requests

--- co2_sensor_scd4x_esp32_http_server.ino
+++ co2_sensor_scd4x_esp32_http_server.ino
@@ -156,7 +156,7 @@ void loop() {
             // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
             // and a content-type so the client knows what's coming, then a blank line:
             client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
+            client.println("Content-type:text/html; charset=UTF-8");
             client.println();
 
             // Read the SCD4X CO2 sensor
@@ -199,14 +199,6 @@ void loop() {
           }
         } else if (c != '\r') {  // if you got anything else but a carriage return character,
           currentLine += c;      // add it to the end of the currentLine
-        }
-
-        // Check to see if the client request was "GET /H" or "GET /L":
-        if (currentLine.endsWith("GET /H")) {
-          digitalWrite(5, HIGH);               // GET /H turns the LED on
-        }
-        if (currentLine.endsWith("GET /L")) {
-          digitalWrite(5, LOW);                // GET /L turns the LED off
         }
       }
     }
